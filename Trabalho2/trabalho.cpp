c560e4a7f94346f37a864342c9390b82d2b13518
Vineww <133378253+Vineww@users.noreply.github.com>
2023-05-15 16:10:35 -0300

    v 1.0

--- Trabalho2/trabalho.cpp
+++ Trabalho2/trabalho.cpp
@@ -1,26 +1,58 @@
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
 //caracteristicas da flor
-struct flores
+struct flower
 {
-float petal_Lenght;
-float petal_Width;
+
+float lenght;
+float width;
 string variety;
-}; flores flor;
+
+}; flower petal;
+
+
 
 //leitura do arquivo csv
 void ler_Excel()
 {
-    ifstream ler("iris_petalas.csv");
+
+ifstream inputFile;
+inputFile.open("iris_petalas.csv");
+
+string line = "";
+while (getline(inputFile, line)) 
+{
+    double length;
+    double width;
+    string variety;
+    string tempString; // ler double
+
+    stringstream inputString(line); // cria um stream fora da linha
+
+    getline(inputString, variety), ',';
 
     
-    while(! ler.eof())
-    {
+    getline(inputString, tempString, ',');
+    width = atoi(tempString.c_str()); // atoi é uma função utilizada para converter variáveis em strings, necessário para usar o getline no width (que é um double).
 
-    }
+    
+    getline(inputString, tempString);
+    length = atof(tempString.c_str()); // convertendo length em string.
+
+    line = "";
+}
+}
+
+//imprimir tabela
+
+void imprime()
+{
+    
 }
 
 //leitura do valor k
